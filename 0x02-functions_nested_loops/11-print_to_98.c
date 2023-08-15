27c5540516bfa492b2c6e5d9fbce19527b8a045b
Ezz-Saied <ezzsaeed459@gmail.com>
2023-08-15 23:06:54 +0300

    ff

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,13 +12,21 @@ void print_to_98(int n)
 	if (n < 98)
 	{
 		for (i = n; i <= 98; i++)
+		{
+			if (i < 98)
 
-			printf("%d, \n", i);
+				printf("%d, ", i);
+		}
 	}
 	else
 	{
 		for (i = n; i >= 98; i--)
+		{
+			if (i > 98)
 
-			printf("%d, \n", i);
+				printf("%d, ", i);
+		}
 	}
+
+	printf('\n')
 }
