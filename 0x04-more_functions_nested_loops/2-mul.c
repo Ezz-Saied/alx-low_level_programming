9700d6f6ea0a8cde29b98548916e64983c77038f
Ezz-Saied <ezzsaeed459@gmail.com>
2023-08-17 18:56:48 +0300

    until 6

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* mul - multiply two numbers
+* @a: first operand
+* @b: second operand
+*
+* Return: the multiplication
+*/
+int mul(int a, int b)
+{
+	int z;
+
+	z = a * b;
+	return (z);
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_line - prints line
+* @n: first operand
+*
+*/
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+		_putchar(95);
+	_putchar('\n');
+}
