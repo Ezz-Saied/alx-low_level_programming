344a30ac8b7cc16bf246b4106f228ffefd0ef33d
Ezz-Saied <ezzsaeed459@gmail.com>
2023-08-17 22:57:13 +0300

    task 0

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,31 +1,15 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-#include <stdio.h>
 #include "main.h"
-/* betty style doc for function main goes there */
 /**
-* positive_or_negative - cheks negative or pisitive
-* @n: first operand
+* positive_or_negative - checks letters in lower case
+* @i: first operand
 *
-* Return: Always 0 (Success)
 */
-void positive_or_negative(int n)
+void positive_or_negative(int i)
 {
-	/*
-	* check if n is positive or nigative or zero
-	*
-	**/
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
 	else
-	{
-		printf("%d is zero\n", n);
-	}
+		printf("%d is zero\n", i);
 }
