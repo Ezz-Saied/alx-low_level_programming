33bd45c648f55157d8ebab4779f27cdd096265f8
Ezz-Saied <ezzsaeed459@gmail.com>
2023-08-28 17:41:44 +0300

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _memset - fills n bytes with value b in base addres of s
+* @s: first operand
+* @b: second operand
+* @n: third operand
+*
+* Return: pointer
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i <= n; i++)
+	{
+
+		*(s + i) = b;
+	}
+
+	return (s);
+}
