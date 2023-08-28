fa938391dc6f365fff5e0d96212cf694520be0a6
Ezz-Saied <ezzsaeed459@gmail.com>
2023-08-28 18:33:29 +0300

    hot fixes

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 /**
 * _strchr - locates a chracter
 * @s: first operand
@@ -13,7 +14,7 @@ char *_strchr(char *s, char c)
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		if (c == *(s + i))
-			return (*(s + i));
+			return (s + i);
 	}
 	return (NULL);
 }
