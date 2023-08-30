d178b5294e53a09d2b6cee5e87e0fb69126bdef1
Ezz-Saied <ezzsaeed459@gmail.com>
2023-08-30 09:44:04 +0300

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _puts_recursion - prints string recursivly
+* @s: first operand
+*
+* Return: 0 or 1
+*/
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s+1);
+	}
+	_putschar('\n');
+}
