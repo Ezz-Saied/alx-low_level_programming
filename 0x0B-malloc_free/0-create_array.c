b06510708a12cd90ebb79e0491bac33b6bc6b9cd
Ezz-Saied <ezzsaeed459@gmail.com>
2023-09-04 17:31:04 +0300

    task 0.2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,10 +13,10 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 
 	if (size == 0)
-		return ('\0');
+		return (NULL);
 	arr = malloc(sizeof(char) * size);
-	if (arr = '\0')
-		return (arr);
+	if (arr == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		*(arr + i) = c;
 	return (arr);
