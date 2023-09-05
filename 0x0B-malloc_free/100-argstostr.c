64b1ece962b2aca4439a219a72bce7d91bc5da47
Ezz-Saied <ezzsaeed459@gmail.com>
2023-09-06 00:08:52 +0300

    task 5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* argstostr - concate arguments
+* @ac: first operand
+* @av: second operand
+*
+* Return: pointer
+*/
+char *argstostr(int ac, char **av)
+{
+	int i;
+	char *arr;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	arr = malloc(sizeof(char **) * ac);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+		arr[i] = *av[i];
+	for (i = 0; i < ac; i++)
+	{
+		_putchar(arr[i]);
+		_putchar('\n');
+	}
+	return (arr);
+}
