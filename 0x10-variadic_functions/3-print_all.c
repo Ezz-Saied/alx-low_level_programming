486c88d660155c5c9f9b4bb567735c8d33d97d77
Ezz-Saied <ezzsaeed459@gmail.com>
2023-09-15 03:21:27 +0300

    task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,46 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+* print_all - sum arguments
+* @format: first operand
+*
+*/
+void print_all(const char * const format, ...)
+{
+	va_list l;
+	char *string;
+	int i;
+
+	i = 0;
+	va_start(l, format);
+	while (format != NULL && format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'i':
+				printf("%i", va_arg(l, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(l, double));
+				break;
+			case 'c':
+				printf("%c", (char) va_arg(l, int));
+				break;
+			case 's':
+				string = va_arg(l, char *);
+				if (string == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", string);
+				break;
+		}
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's') && format[i + 1] != '\0')
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	va_end(l);
+}
